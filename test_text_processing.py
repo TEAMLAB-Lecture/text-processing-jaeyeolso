ad0de203750fcd3bf3729b139e4b4fbc464e232c
jaeyeolso <wodufdl48@naver.com>
2021-01-19 16:31:45 +0900

    Update my code

--- test_text_processing.py
+++ test_text_processing.py
@@ -91,6 +91,3 @@ class TestTextProcessing(unittest.TestCase):
         test_str = "abcdefghijklmnopqrstuvwxyz"
         pred = tp.no_vowels(test_str)
         self.assertEqual(pred, "bcdfghjklmnpqrstvwxyz")
-
-if __name__ == "__main__":
-    unittest.main()
